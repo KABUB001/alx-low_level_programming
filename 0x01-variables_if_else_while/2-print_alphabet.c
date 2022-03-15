b7169bcc5c478617f5560f66af5a1e37e2f81cfc
KABUB001 <berangerkoffi98@gmail.com>
2022-03-14 23:01:24 -0700

    C variables,if,else,while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int lower_n = tolower(n);
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		putchar(lower_n);
+	}
+	return (0);
+}
