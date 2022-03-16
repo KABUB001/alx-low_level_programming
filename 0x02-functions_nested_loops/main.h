c162a89ae9dea17fca5882fa3ce116659510e9d3
KABUB001 <berangerkoffi98@gmail.com>
2022-03-15 22:36:53 -0700

    C functions,nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - use printf function
+ *
+ * Return: Always 0 (success)
+ */
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch='a'; ch <= 'z'; ch++)
+	{
+		return (_putchar(ch\n));
+	}	
+
+
+}
