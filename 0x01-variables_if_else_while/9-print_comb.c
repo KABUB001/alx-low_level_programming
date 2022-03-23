c0e01968a21fae637fc3bf8507ecdc18e55e5451
KABUB001 <berangerkoffi98@gmail.com>
2022-03-23 02:28:58 -0700

    C variables,if,else,while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 8; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n != 9)
+		{
+			putchar(',');
+		        putchar(' ');
+		}
 	}
-	putchar(9 + '\n');
+	putchar('\n');
 	return (0);
 }
