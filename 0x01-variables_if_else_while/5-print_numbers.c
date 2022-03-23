d825bf7e5bbb62e45079666cab9bcd803ffdd3ad
KABUB001 <berangerkoffi98@gmail.com>
2022-03-23 00:24:33 -0700

    C variables,if,else,while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function goeas there */
+/**
+ * main - print 0 to 9 followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+        putchar('\n');
+	return (0);
+}
