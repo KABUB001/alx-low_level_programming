4e6584c77bb6ff51754b44221f77c7d627fed76d
KABUB001 <berangerkoffi98@gmail.com>
2022-03-23 01:38:50 -0700

    C variables,if,else,while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function goes there*/
+/**
+ * main - print 0 to 9 separated by "," and space
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <=9; n++)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
