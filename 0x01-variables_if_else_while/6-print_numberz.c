ac3be0c49227fb21c82974c4e0a050442076e958
KABUB001 <berangerkoffi98@gmail.com>
2022-03-23 00:39:14 -0700

    C variables,if,else,while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function goes there */
+/**
+ * main - print 0 to 9 using putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
