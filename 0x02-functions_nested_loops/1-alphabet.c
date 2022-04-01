c424174e47049ac9720b42cfc25c46010490c3d1
KABUB001 <berangerkoffi98@gmail.com>
2022-03-31 19:27:41 -0700

    C functions,nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - prints alphabet a to z
  *
  * Return: Always 0
  */
@@ -9,7 +9,9 @@ int print_alphabet(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-{_putchar(ch); }	
+	{
+		_putchar(ch);
+	}	
 	_putchar('\n');
 	return (0);
 
