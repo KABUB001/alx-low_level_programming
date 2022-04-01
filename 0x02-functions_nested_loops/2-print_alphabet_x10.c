b95b106ed9321e1acdd45ddfa851206933c60e47
KABUB001 <berangerkoffi98@gmail.com>
2022-03-31 20:10:55 -0700

    C functions,nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Retuen: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (a = 1; a <= 10; a++)
+	{
+		void print_alphabet(void);
+	}
+}
