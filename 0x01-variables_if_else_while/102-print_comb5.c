5792ae87ef081857c2db5df1c6484cab985dad92
KABUB001 <berangerkoffi98@gmail.com>
2022-04-02 09:41:09 -0700

    C variables,if,else,while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print combination of four digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0 ; b <= 9; b++)
+		{
+			for (c = a; c <= 9; c++)
+			{
+				for (d = b + 1; d <= 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+				        putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
