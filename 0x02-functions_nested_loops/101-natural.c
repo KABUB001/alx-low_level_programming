295459c2dbf482d74824d84ed94e6fd826fabd1e
KABUB001 <berangerkoffi98@gmail.com>
2022-04-03 16:13:35 -0700

    C functions,nested_loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - computes and prints
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 1024 / 3;
+	int b = 1024 / 5;
+	int x;
+	int y;
+	int c = 0;
+	int d = 0;
+
+	for (x = 1; x <= a; x++)
+	{
+		c += x * 3;
+	}
+
+	for (y = 1; y <= b; y++)
+	{
+		d += y * 5;
+	}
+
+	int res = c + d;
+
+	printf("%d", res);
+}
