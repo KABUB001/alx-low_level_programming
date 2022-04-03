b60ff9dd8f4d3160ffcb491a44c041077371dc3f
KABUB001 <berangerkoffi98@gmail.com>
2022-04-03 10:49:32 -0700

    C functions,nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,20 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d\n", n);
+			printf("%d\, ", n);
 			n--;
 		}
+
+		printf("\n");
 	}
 	else
 	{
 		while (n <= 98)
 		{
-			printf("%d\n", n);
+			printf("%d\, ", n);
 			n++;
 		}
+
+		printf("\n");
 	}
 }
