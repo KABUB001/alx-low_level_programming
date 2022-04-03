639c6cd18eab46670dfbbef80c4a41a2ab9657a5
KABUB001 <berangerkoffi98@gmail.com>
2022-04-03 06:06:39 -0700

    C functions,nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <main.h>
+/**
+ * print_sign - print the sign of a number
+ *
+ * @n: character to test
+ *
+ * Return: 1 if 'n' is positive , -1 if it's negative, 0 if 'n' is zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
